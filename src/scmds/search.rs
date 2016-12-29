88ae68c54287e0565079f0e423a5f0b257c7ea77
Sebastian Thiel <byronimo@gmail.com>
2016-12-29 17:41:01 +0100

    test(search): use mpsc instead
    Current version does not compile because ... hmm.
    I really don't get it.

--- src/scmds/search.rs
+++ src/scmds/search.rs
@@ -1,11 +1,8 @@
 extern crate curl;
 extern crate termion;
 extern crate futures;
-extern crate futures_cpupool;
 extern crate tokio_core;
 extern crate tokio_curl;
-
-use self::futures_cpupool::CpuPool;
 
 use clap;
 use std::thread;
@@ -17,7 +14,8 @@ use self::termion::input::TermRead;
 use self::termion::clear;
 use self::termion::cursor;
 use self::tokio_core::reactor::Core;
-use self::futures::Future;
+use self::futures::{Sink, Stream, Future};
+use self::futures::sync::mpsc;
 use self::tokio_curl::Session;
 use std::io::{self, Write};
 
@@ -28,10 +26,40 @@ pub fn handle_interactive_search(_args: &clap::ArgMatches) {
     let mut stdout = ok_or_exit(io::stdout().into_raw_mode());
     ok_or_exit(write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All));
     let mut term = String::new();
-    let pool = CpuPool::new(1);
-    let mut most_recent_search = None;
-    let reactor = ok_or_exit(Core::new());
-    let session = Session::new(reactor.handle());
+    let (mut sender, receiver) = mpsc::channel(4);
+    thread::spawn(|| {
+        let reactor = ok_or_exit(Core::new());
+        let session = Session::new(reactor.handle());
+        let search_terms = receiver.and_then(|term| {
+                ok_or_exit(write!(io::stdout(),
+                                  "{}{}{}searching {}",
+                                  cursor::Hide,
+                                  cursor::Goto(1, 2),
+                                  clear::CurrentLine,
+                                  term));
+                let mut req = Easy::new();
+                req.get(true).unwrap();
+                req.url("https://www.rust-lang.org").unwrap();
+                req.write_function(|data| {
+                        write!(io::stdout(), "{}{}", cursor::Hide, cursor::Goto(1, 2)).unwrap();
+                        io::stdout().write_all(data).unwrap();
+                        Ok(data.len())
+                    })
+                    .unwrap();
+                session.perform(req)
+            })
+            .for_each(|response| {
+                ok_or_exit(write!(io::stdout(),
+                                  "{}{}{}    {} done !",
+                                  cursor::Hide,
+                                  cursor::Goto(1, 2),
+                                  clear::CurrentLine,
+                                  term));
+                io::stdout().flush().ok();
+                Ok(())
+            });
+        reactor.run(search_terms);
+    });
 
     for k in stdin.keys() {
         match ok_or_exit(k) {
@@ -47,41 +75,12 @@ pub fn handle_interactive_search(_args: &clap::ArgMatches) {
             _ => println!("unsupported!"),
         }
         ok_or_exit(write!(stdout,
-                          "{}{}{}{}",
+                          "{}{}{}crates.io search: {}",
                           cursor::Show,
                           cursor::Goto(1, 1),
                           clear::CurrentLine,
                           term));
-        let term_owned = term.clone();
-        ok_or_exit(write!(io::stdout(),
-                          "{}{}{}searching {}",
-                          cursor::Hide,
-                          cursor::Goto(1, 2),
-                          clear::CurrentLine,
-                          term_owned));
-        let mut req = Easy::new();
-        req.get(true).unwrap();
-        req.url("https://www.rust-lang.org").unwrap();
-        req.write_function(|data| {
-            write!(io::stdout(), "{}{}",
-            cursor::Hide,
-            cursor::Goto(1, 2)).unwrap();
-            io::stdout().write_all(data).unwrap();
-            Ok(data.len())
-        }).unwrap();
-        let req = session.perform(req)
-            .and_then(move |_| {
-                ok_or_exit(write!(io::stdout(),
-                                  "{}{}{}    {} done !",
-                                  cursor::Hide,
-                                  cursor::Goto(1, 2),
-                                  clear::CurrentLine,
-                                  term_owned));
-                io::stdout().flush().ok();
-                Ok(())
-            });
-        most_recent_search = Some(pool.spawn(req));
         stdout.flush().ok();
+        sender.send(term.clone());
     }
-    drop(most_recent_search);
 }
