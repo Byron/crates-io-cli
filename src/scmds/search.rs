3f48d91677583af663be842f302ab6e6d96f88c0
Sebastian Thiel <byronimo@gmail.com>
2016-12-29 15:02:08 +0100

    refactor(search): rustfmt and better arrangement of timer

--- src/scmds/search.rs
+++ src/scmds/search.rs
@@ -20,15 +20,17 @@ use utils::ok_or_exit;
 pub fn handle_interactive_search(_args: &clap::ArgMatches) {
     let stdin = io::stdin();
     let mut stdout = ok_or_exit(io::stdout().into_raw_mode());
-    ok_or_exit(write!(stdout, "{}", clear::All));
+    ok_or_exit(write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All));
     let mut term = String::new();
     let pool = CpuPool::new(1);
     let mut most_recent_search = None;
+    let interruptable_timer = tokio_timer::Timer::default();
+
     for k in stdin.keys() {
         match ok_or_exit(k) {
             Key::Char(c) => {
-              term.push(c);
-            },
+                term.push(c);
+            }
             Key::Backspace => {
                 term.pop();
             }
@@ -37,16 +39,30 @@ pub fn handle_interactive_search(_args: &clap::ArgMatches) {
             }
             _ => println!("unsupported!"),
         }
-        ok_or_exit(write!(stdout, "{}{}{}{}", cursor::Show, cursor::Goto(1, 1), clear::CurrentLine,  term));
+        ok_or_exit(write!(stdout,
+                          "{}{}{}{}",
+                          cursor::Show,
+                          cursor::Goto(1, 1),
+                          clear::CurrentLine,
+                          term));
         let term_owned = term.clone();
-        ok_or_exit(write!(io::stdout(), "{}{}{}searching {}", cursor::Hide, cursor::Goto(1, 2), clear::CurrentLine,  term_owned));
-        let interruptable_timer = tokio_timer::Timer::default();
-        let waiter = interruptable_timer.sleep(std::time::Duration::from_secs(2));
-        let waiter = waiter.and_then(move |_| {
-            ok_or_exit(write!(io::stdout(), "{}{}{}    {} done !", cursor::Hide, cursor::Goto(1, 2), clear::CurrentLine, term_owned));
-            io::stdout().flush().ok();
-            Ok(())
-        });
+        ok_or_exit(write!(io::stdout(),
+                          "{}{}{}searching {}",
+                          cursor::Hide,
+                          cursor::Goto(1, 2),
+                          clear::CurrentLine,
+                          term_owned));
+        let waiter = interruptable_timer.sleep(std::time::Duration::from_secs(2))
+            .and_then(move |_| {
+                ok_or_exit(write!(io::stdout(),
+                                  "{}{}{}    {} done !",
+                                  cursor::Hide,
+                                  cursor::Goto(1, 2),
+                                  clear::CurrentLine,
+                                  term_owned));
+                io::stdout().flush().ok();
+                Ok(())
+            });
         most_recent_search = Some(pool.spawn(waiter));
         stdout.flush().ok();
     }
