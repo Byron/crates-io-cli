53ac3ec6a6d6a063abfd195469cef3d45051c047
Sebastian Thiel <byronimo@gmail.com>
2016-12-29 12:33:25 +0100

    test(cli): minimal read-line like functionality

--- src/scmds/search.rs
+++ src/scmds/search.rs
@@ -4,6 +4,8 @@ use clap;
 use self::termion::event::Key;
 use self::termion::raw::IntoRawMode;
 use self::termion::input::TermRead;
+use self::termion::clear;
+use self::termion::cursor;
 use std::io::{self, Write};
 
 use utils::ok_or_exit;
@@ -11,12 +13,18 @@ use utils::ok_or_exit;
 pub fn handle_interactive_search(_args: &clap::ArgMatches) {
     let stdin = io::stdin();
     let mut stdout = ok_or_exit(io::stdout().into_raw_mode());
+    let mut term = String::new();
     for k in stdin.keys() {
         match ok_or_exit(k) {
-            Key::Char('q') => break,
-            Key::Char(c) => print!("{}", c),
+            Key::Char(c) => {
+              term.push(c);
+            },
+            Key::Backspace => {
+                term.pop();
+            }
             _ => println!("unsupported!"),
         }
+        write!(stdout, "{}{}{}", clear::All, cursor::Goto(1, 1), term).unwrap();
         stdout.flush().ok();
     }
 }
