0e8837808812ac9807005aa96e6683fbd7081592
Sebastian Thiel <byronimo@gmail.com>
2016-12-30 17:06:14 +0100

    refactor(search): allow to send commands instead
    This basically offloads all work related to the content
    into a separate thread powered by futures.

--- src/scmds/search/interactive.rs
+++ src/scmds/search/interactive.rs
@@ -12,7 +12,7 @@ use termion::input::TermRead;
 use termion::clear;
 use termion::cursor;
 use tokio_core::reactor::Core;
-use futures::{Sink, Stream, Future};
+use futures::{self, Sink, Stream, Future};
 use futures::sync::mpsc;
 use tokio_curl::Session;
 use std::io::{self, Write};
@@ -28,26 +28,30 @@ fn dimension() -> Dimension {
     Dimension::default().loose_heigth(NON_CONTENT_LINES)
 }
 
+#[derive(Clone)]
+enum Command {
+    Search(String),
+    Clear,
+}
+
 #[derive(Clone, Copy)]
 enum Mode {
-    Searching
+    Searching,
 }
 
+impl Default for Mode {
+    fn default() -> Self {
+        Searching
+    }
+}
+
+use self::Command::*;
 use self::Mode::*;
 
-#[derive(Clone)]
-struct State  {
+#[derive(Default)]
+struct State {
+    term: String,
     mode: Mode,
-    term: String
-}
-
-impl Default for State {
-    fn default() -> State {
-       State {
-           mode: Searching,
-          term: String::new()
-       }
-    }
 }
 
 pub fn handle_interactive_search(_args: &clap::ArgMatches) {
@@ -64,60 +68,81 @@ pub fn handle_interactive_search(_args: &clap::ArgMatches) {
         let session = Session::new(reactor.handle());
         let mut current_result = None;
 
-        let operations = receiver.and_then(|state: State| {
-            match state.mode {
-                Searching => {
-                    let mut req = Easy::new();
-                    let dim = dimension();
-                    ok_or_exit(req.get( true ));
-                    let url = format !("https://crates.io/api/v1/crates?page=1&per_page={}&q={}&sort=",
-                                       dim.height,
-                                       req.url_encode(String::as_bytes( &state.term)));
-                    ok_or_exit(req.url( & url));
-                    let buf = Arc::new(Mutex::new(Vec::new()));
-                    let buf_handle = buf.clone();
-                    ok_or_exit(req.write_function( move | data | {
-                        buf_handle.lock().unwrap().extend_from_slice(data);
-                        Ok(data.len())
-                    }));
-                    info( & "searching ...");
-                    session.perform(req)
-                           .map_err( | e| {
-                               info( & e);
-                               ()
-                           })
-                           .map( move | _response | {
-                               let buf_slice = buf.lock().unwrap();
-                               let result = SearchResult::from_data(& buf_slice, dim).map_err( | e| {
-                                   write ! (io::stderr(), "{}\n", String::from_utf8_lossy( & buf_slice)).ok();
-                                   e
-                               });
-                               result
-                           })
-                }}
+        let commands = receiver.and_then(|cmd: Command| {
+                match cmd.clone() {
+                    Clear => {
+                        futures::finished((SearchResult::with_dimension(dimension()), cmd)).boxed()
+                    }
+                    Search(term) => {
+                        let mut req = Easy::new();
+                        let dim = dimension();
+                        ok_or_exit(req.get(true));
+                        let url = format!("https://crates.\
+                                           io/api/v1/crates?page=1&per_page={}&q={}&sort=",
+                                          dim.height,
+                                          req.url_encode(String::as_bytes(&term)));
+                        ok_or_exit(req.url(&url));
+                        let buf = Arc::new(Mutex::new(Vec::new()));
+                        let buf_handle = buf.clone();
+                        ok_or_exit(req.write_function(move |data| {
+                            buf_handle.lock().unwrap().extend_from_slice(data);
+                            Ok(data.len())
+                        }));
+                        info(&"searching ...");
+                        session.perform(req)
+                            .map_err(|e| {
+                                info(&e);
+                                ()
+                            })
+                            .map(move |_response| {
+                                let buf_slice = buf.lock().unwrap();
+                                let result = SearchResult::from_data(&buf_slice, dim)
+                                    .map_err(|e| {
+                                        write!(io::stderr(),
+                                               "{}\n",
+                                               String::from_utf8_lossy(&buf_slice))
+                                            .ok();
+                                        e
+                                    });
+                                (ok_or_exit(result), cmd)
+                            })
+                            .boxed()
+                    }
+                }
             })
-            .for_each(|search| {
-                let search: SearchResult = ok_or_exit(search);
-                info(&format!("{} results in total, showing {} max",
-                              search.meta.total,
-                              search.meta.dimension.as_ref().expect("dimension to be set").height));
-                if search.crates.is_empty() {
-                    let last = usage();
-                    write!(io::stdout(),
-                           "{gotolast} - 0 results found",
-                           gotolast = cursor::Goto(last as u16, INFO_LINE.1))
-                        .ok();
-                } else {
-                    write!(io::stdout(), "{goto}{}", search, goto = CONTENT_LINE).ok();
-                    current_result = Some(search);
+            .for_each(|(search, cmd)| {
+                match cmd {
+                    Clear => {
+                        usage();
+                        write!(io::stdout(), "{goto}{}", search, goto = CONTENT_LINE).ok();
+                    }
+                    Search(_term) => {
+                        info(&format!("{} results in total, showing {} max",
+                                      search.meta.total,
+                                      search.meta
+                                          .dimension
+                                          .as_ref()
+                                          .expect("dimension to be set")
+                                          .height));
+                        if search.crates.is_empty() {
+                            let last = usage();
+                            write!(io::stdout(),
+                                   "{gotolast} - 0 results found",
+                                   gotolast = cursor::Goto(last as u16, INFO_LINE.1))
+                                .ok();
+                        } else {
+                            write!(io::stdout(), "{goto}{}", search, goto = CONTENT_LINE).ok();
+                            current_result = Some(search);
+                        }
+                    }
                 }
                 io::stdout().flush().ok();
                 Ok(())
             });
-        reactor.run(operations).ok();
+        reactor.run(commands).ok();
     });
 
-    let mut ongoing_search = None;
+    let mut ongoing_command = None;
     let mut state = State::default();
     let pool = CpuPool::new(1);
 
@@ -143,21 +168,16 @@ pub fn handle_interactive_search(_args: &clap::ArgMatches) {
                     }
                 }
                 promptf(&state.term, &mut stdout);
-                if state.term.is_empty() {
-                    usage();
-                    write!(stdout,
-                           "{goto}{}",
-                           SearchResult::with_dimension(dimension()),
-                           goto = CONTENT_LINE)
-                        .ok();
-                    stdout.flush().ok();
+                let cmd = if state.term.is_empty() {
+                    Clear
                 } else {
-                    ongoing_search = Some(pool.spawn(sender.clone().send(state.clone())));
-                }
+                    Search(state.term.clone())
+                };
+                ongoing_command = Some(pool.spawn(sender.clone().send(cmd)));
             }
         }
     }
-    drop(ongoing_search);
+    drop(ongoing_command);
     drop(sender);
     t.join().unwrap();
     reset_terminal();
