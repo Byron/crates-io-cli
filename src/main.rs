49a13b986d6b941bf7c443df5edc4164769d10a2
Sebastian Thiel <byronimo@gmail.com>
2017-01-26 12:06:45 +0100

    remove lib
    It's not really needed after all, and bringing it back is easy.
    Note: it was added in the first place to write some rust unit tests,
    which in the end were easier to implement just as unit-test within
    the respective module.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 #[macro_use]
 extern crate clap;
-extern crate crates_io_cli;
+#[macro_use]
+extern crate prettytable;
+extern crate rustc_serialize;
+extern crate crates_index_diff;
+extern crate futures;
+extern crate futures_cpupool;
+extern crate curl;
+extern crate tokio_core;
+extern crate tokio_curl;
+#[cfg(unix)]
+extern crate termion;
+extern crate open;
+#[macro_use]
+extern crate quick_error;
+extern crate git2;
+extern crate urlencoding;
 
-use crates_io_cli::{ok_or_exit, handle_interactive_search, handle_recent_changes, handle_list,
-                    by_user, OutputKind};
+mod utils;
+mod scmds;
+mod structs;
+
+use utils::ok_or_exit;
+use scmds::{handle_interactive_search, handle_recent_changes, handle_list, by_user, OutputKind};
 
 use std::env;
 use std::path::PathBuf;
