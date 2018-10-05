85cb942ad1b829a79f59a46984aaf5fbea7981b5
Sebastian Thiel <sthiel@thoughtworks.com>
2018-10-05 11:10:03 +0200

    Potentially fix tests

--- bin/test-cli.sh
+++ bin/test-cli.sh
@@ -4,11 +4,6 @@ exe=${1:?First argument is the executable to test}
 existing_crates_clone=${2:?Second argument is a pre-existing clone of crates.io}
 
 
-if $exe >/dev/null; then
-  echo "Invocation without any subcommand is a failure, but prints usage"
-  exit 1
-fi
-
 with_repo="--repository $existing_crates_clone"
 if ! $exe recent-changes $with_repo >/dev/null; then
   echo "Expecting recent-changes on existing repo to work"
