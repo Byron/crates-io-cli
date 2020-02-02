7fab5b937a20cc66b1547ce20bd5622a3f6809d9
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-02-02 15:59:41 +0800

    Add disclaimer - let's not fix the old stuff
    It's too cumbersome, and there is no value given I can try new things
    while solving new problems.
    
    [skip CI]

--- src/scmds/search/mod.rs
+++ src/scmds/search/mod.rs
@@ -1,3 +1,8 @@
+//! If you are seeing this, than you see a very old way of doing things, prior
+//! to 'async' being a keyword, and while all async was in its infancy.
+//! Even though it would be nice to see how that code would look nowadays, I
+//! would rather solve new problems than changing existing solutions, despite them
+//! being pretty complex for what they do.
 #[cfg(unix)]
 mod interactive;
 #[cfg(unix)]
