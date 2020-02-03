ed70ea0b7d38e0a81f744063d1c2da910db884e2
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-02-03 19:14:08 +0800

    refactor

--- criner/src/engine.rs
+++ criner/src/engine.rs
@@ -8,6 +8,50 @@ use crates_index_diff::Index;
 use log::info;
 use std::{path::Path, time::SystemTime};
 
+async fn process_changes(
+    db: Db,
+    crates_io_path: impl AsRef<Path>,
+    deadline: Option<SystemTime>,
+) -> Result<()> {
+    info!("Potentially cloning crates index - this can take a while…");
+    let index = enforce_blocking(deadline, {
+        let path = crates_io_path.as_ref().to_path_buf();
+        || Index::from_path_or_cloned(path)
+    })
+    .await??;
+    info!("Fetching crates index to see changes");
+    let crate_versions = enforce_blocking(deadline, move || index.fetch_changes()).await??;
+
+    info!("Fetched {} changed crates", crate_versions.len());
+    let check_interval = std::cmp::max(crate_versions.len() / 100, 1);
+    enforce_blocking(deadline, {
+        let db = db.clone();
+        move || {
+            let meta = db.open_crate_versions()?;
+            let krate = db.open_crates()?;
+            // NOTE: this loop can also be a stream, but that makes computation slower due to overhead
+            // Thus we just do this 'quickly' on the main thread, knowing that criner really needs its
+            // own executor or resources.
+            // We could chunk things, but that would only make the code harder to read. No gains here…
+            // NOTE: Even chunks of 1000 were not faster, didn't even saturate a single core...
+            for (versions_stored, version) in crate_versions.iter().enumerate() {
+                meta.insert(&version)?;
+                krate.insert_version(&version)?;
+                if versions_stored % check_interval == 0 {
+                    info!(
+                        "Stored {} of {} crate versions in database",
+                        versions_stored + 1,
+                        crate_versions.len()
+                    );
+                }
+            }
+            Ok::<_, Error>(())
+        }
+    })
+    .await??;
+    Ok(())
+}
+
 /// Runs the statistics and mining engine.
 /// May run for a long time unless a deadline is specified.
 /// Even though timeouts can be achieved from outside of the future, knowing the deadline may be used
@@ -19,48 +63,9 @@ pub async fn run(
 ) -> Result<()> {
     let start_of_computation = SystemTime::now();
     check(deadline)?;
-    let res = async {
-        info!("Potentially cloning crates index - this can take a while…");
-        let index = enforce_blocking(deadline, {
-            let path = crates_io_path.as_ref().to_path_buf();
-            || Index::from_path_or_cloned(path)
-        })
-        .await??;
-        let db = Db::open(db)?;
 
-        info!("Fetching crates index to see changes");
-        let crate_versions = enforce_blocking(deadline, move || index.fetch_changes()).await??;
-
-        info!("Fetched {} changed crates", crate_versions.len());
-        let check_interval = std::cmp::max(crate_versions.len() / 100, 1);
-        enforce_blocking(deadline, {
-            let db = db.clone();
-            move || {
-                let meta = db.open_crate_versions()?;
-                let krate = db.open_crates()?;
-                // NOTE: this loop can also be a stream, but that makes computation slower due to overhead
-                // Thus we just do this 'quickly' on the main thread, knowing that criner really needs its
-                // own executor or resources.
-                // We could chunk things, but that would only make the code harder to read. No gains here…
-                // NOTE: Even chunks of 1000 were not faster, didn't even saturate a single core...
-                for (versions_stored, version) in crate_versions.iter().enumerate() {
-                    meta.insert(&version)?;
-                    krate.insert_version(&version)?;
-                    if versions_stored % check_interval == 0 {
-                        info!(
-                            "Stored {} of {} crate versions in database",
-                            versions_stored + 1,
-                            crate_versions.len()
-                        );
-                    }
-                }
-                Ok::<_, Error>(())
-            }
-        })
-        .await??;
-        Ok(())
-    }
-    .await;
+    let db = Db::open(db)?;
+    let res = process_changes(db, crates_io_path, deadline).await;
     info!(
         "Wallclock elapsed: {}",
         humantime::format_duration(
