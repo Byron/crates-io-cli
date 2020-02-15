6e3459d35de6ed31ba82521a68603d45ee8da9ab
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-02-15 13:46:43 +0800

    Only show hours minutes seconds

--- progress-dashboard/src/tui/draw/messages.rs
+++ progress-dashboard/src/tui/draw/messages.rs
@@ -9,7 +9,8 @@ use tui::{
     widgets::{Block, Borders, Widget},
 };
 
-const TIME_COLUMN_WIDTH: u16 = "2020-02-13T00:51:45".len() as u16;
+const TIME_COLUMN_PREFIX: u16 = "20-02-13T".len() as u16;
+const TIME_COLUMN_SUFFIX: u16 = "00:51:45".len() as u16;
 
 pub fn pane(messages: &[Message], bound: Rect, buf: &mut Buffer) {
     let mut block = Block::default().title("Messages").borders(Borders::TOP);
@@ -35,8 +36,10 @@ fn format_time_column(time: &SystemTime) -> String {
     format!(
         "{}{}",
         String::from_utf8_lossy(
-            &format!("{}", humantime::format_rfc3339(*time),).as_bytes()
-                [..TIME_COLUMN_WIDTH as usize],
+            &format!("{}", humantime::format_rfc3339_seconds(*time)).as_bytes()[(TIME_COLUMN_PREFIX
+                + 2)
+                as usize
+                ..(TIME_COLUMN_PREFIX + TIME_COLUMN_SUFFIX + 2) as usize],
         ),
         rect::VERTICAL_LINE
     )
@@ -45,7 +48,7 @@ fn format_time_column(time: &SystemTime) -> String {
 fn compute_bounds(line: Rect) -> (Option<Rect>, Rect) {
     let vertical_line_width = 1u16;
     let time_bound = Rect {
-        width: TIME_COLUMN_WIDTH + vertical_line_width,
+        width: TIME_COLUMN_SUFFIX + vertical_line_width,
         ..line
     };
     let message_bound = rect::intersect(rect::offset_x(line, time_bound.width), line);
